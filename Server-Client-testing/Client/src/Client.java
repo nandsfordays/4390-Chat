b9178971256993ffd0f4f78c4a6eabc710fa3e8d
RadicalDuck <shahs135@gmail.com>
2019-04-23 19:26:52 -0500

    Working chat system, however client needs to send first

--- Server-Client-testing/Client/src/Client.java
+++ Server-Client-testing/Client/src/Client.java
@@ -1,3 +1,5 @@
+//Saurabh Shah
+
 
 import java.net.*;
 import java.io.*;
@@ -5,9 +7,116 @@ import java.util.Scanner;
 
 public class Client {
 	
+	public static void main(String[] args) throws Exception 
+	{
+		//reads port number
+		Scanner scan = new Scanner(System.in);
+		int portnum = -1;
+		String msg = "";
+		int state = -1;
+		
+		try
+		{
+			System.out.println("Please enter an integer for the port number.");
+			portnum = scan.nextInt();
+			scan.nextLine();
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error reading port number. Please enter an integer.");
+		}
+		
+		
+		//ON LEVEL - server and client are equals
+        try (Socket socket = new Socket("127.0.0.1", portnum)) 
+        {
+            System.out.println("Enter lines of text");  
+            
+            Scanner in = new Scanner(socket.getInputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            
+            //handler - loops and checks for input on console and server
+            while (state != 0) 
+            {
+            	if(scan.hasNextLine()) //reading from console and sending to server
+                {
+                	state = 1; 
+                	//read from console
+                	msg = scan.nextLine();
+                	
+                	//ignore empty strings
+                	if(msg.equals(""))
+            		{
+            			continue;
+            		}
+                	
+                	//send to server
+                    out.println(msg);
+                    
+                    //exit condition
+                    if(msg.equals(".over"))
+            		{
+            			break;
+            		}
+                    
+                }
+            	
+            	if(in.hasNextLine()) //reading from server and sending to console
+                {
+                	state = 2; 
+                	//read from server
+                	msg = in.nextLine();
+                	
+                	//ignore empty string
+                	if(msg.equals(""))
+            		{
+            			continue;
+            		}
+                	
+                	//send to console
+                    System.out.println(msg);
+                    
+                    //exit condition
+                    if(msg.equals(".over"))
+            		{
+            			break;
+            		}
+                    
+                }
+            	
+            }
+            in.close();
+        }
+        catch(Exception e)
+		{
+			System.out.println("Error :" + e.getMessage());
+		}
+        
+        scan.close();
+        
+        
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	
 	private Socket socket = null;
 	private Scanner streamIn = null;
 	private DataOutputStream streamOut = null;
+	private DataInputStream socketIn = null;
 	
 	public static void main(String args[])
 	{
@@ -35,6 +144,7 @@ public class Client {
 			socket = new Socket(address,port);
 			streamIn = new Scanner(System.in);
 			streamOut = new DataOutputStream(socket.getOutputStream());
+			socketIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
 			
 			//write to socket
 			String msg = "";
@@ -48,10 +158,19 @@ public class Client {
 						msg = streamIn.next();
 						streamOut.writeUTF(msg);
 					}
+					else //hard coded message in case read fails
+					{
+						msg = "error reading string";
+						streamOut.writeUTF(msg);
+					}
+					
+					//reading from server
+					msg = socketIn.readUTF();
+					System.out.println(msg);
 				}
 				catch(Exception e)
 				{
-					System.out.println("Error reading port number. Please enter an integer.");
+					System.out.println("Error :" + e.getMessage());
 				}
 			}
 			
@@ -78,5 +197,6 @@ public class Client {
 		
 	}
 	
+	*/
 	
 }
--- Server-Client-testing/Server/src/Server.java
+++ Server-Client-testing/Server/src/Server.java
@@ -1,14 +1,197 @@
+//Saurabh Shah
+
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class Server {
 
+	//runs firsts, starts up the executor and reads port number
+	
+    public static void main(String[] args) throws Exception 
+    {
+    	//reads port number
+    	Scanner scan = new Scanner(System.in);
+    	int portnum = -1;
+    	
+    	try
+    	{
+    		System.out.println("Please enter an integer for the port number.");
+    		portnum = scan.nextInt();
+    		
+    	}
+    	catch(Exception e)
+    	{
+    		System.out.println("Error reading port number. Please enter an integer.");
+    	}
+    	
+    	
+    	//initialize ServerSocket and start thread
+        try
+        {
+        	ServerSocket server = new ServerSocket(portnum);
+        	Socket client = null;
+        	
+            System.out.println("The server is running...");
+            ExecutorService pool = Executors.newFixedThreadPool(1);
+            
+            //string msg for holding messages between client and server
+            String msg = "";
+            
+            while (true) 
+            {
+            	//sends socket to ChatServer
+            	client = server.accept();
+                pool.execute(new ChatServer(client));
+                
+            }
+            
+        }
+        catch(Exception e)
+        {
+ 		   	System.out.println("Error :" + e.getMessage());
+        }
+    }
+
+    
+    
+    private static class ChatServer implements Runnable {
+        private Socket socket;
+        int state = -1; //state does absolutely nothing, but it helped with debugging
+        
+        //reading from console
+        Scanner console = new Scanner(System.in);
+        
+        //constructor
+        public ChatServer(Socket socket) 
+        {
+            this.socket = socket;
+        }
+
+        //thread part. ON LEVEL - both server and client should be equals
+        @Override
+        public void run() 
+        {
+        	System.out.println("Connected: " + socket);
+        	String msg = "";
+            try 
+            {
+                Scanner in = new Scanner(socket.getInputStream());
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                
+                //handler. Loops until exit condition is reached
+                while (state != 0) 
+                {
+                	if(in.hasNextLine()) //read from client and send to console
+                	{
+                		state = 1;
+                		//read from client
+                		msg = in.nextLine();
+                    	
+                		//nullifies empty strings
+                		if(msg.equals(""))
+                		{
+                			continue;
+                		}
+                		
+                		
+                    	//writes to console
+                    	System.out.println(msg);
+                    	
+                    	//exit condition
+                    	if(msg.equals(".over"))
+                		{
+                			break;
+                		}
+                    	
+                	}
+                	
+                	if(console.hasNextLine()) //read from console and send to client
+                	{
+                		state = 2;
+                		//read from console
+                		msg = console.nextLine();
+                		
+                		//nullifies empty strings
+                		if(msg.equals(""))
+                		{
+                			continue;
+                		}
+                		
+                		//sends to client
+                		out.println(msg);
+                		
+                		//exit condition
+                		if(msg.equals(".over"))
+                		{
+                			break;
+                		}
+                		
+                	}
+                	
+                	
+                }
+                
+                in.close(); //is this needed?
+            } 
+            catch (Exception e) 
+            {
+                System.out.println("Error:" + socket);
+            } 
+            finally 
+            {
+                try 
+                {
+                	socket.close(); 
+                	
+                } 
+                catch (IOException e) 
+                {
+                	
+                }
+                
+                System.out.println("Closed: " + socket);
+            }
+        }
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	
 	private ServerSocket server = null;
 	private Socket client = null;
 	private DataInputStream streamIn = null;
 	private DataOutputStream streamOut = null;
-	
 	
 	public static void main(String args[])
 	{
@@ -38,7 +221,8 @@ public class Server {
 			client = server.accept();
 			System.out.println("client accepted");
 			streamIn = new DataInputStream(new BufferedInputStream(client.getInputStream()));
-		
+			streamOut = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+			
 			//reading from client
 			String msg = "";
 			
@@ -48,10 +232,14 @@ public class Server {
 				{
 					msg = streamIn.readUTF();
 					System.out.println(msg);
+					
+					//writing message back
+					streamOut.writeUTF(msg.toUpperCase());
 				}
 	            catch(IOException i) 
 	            { 
 	                System.out.println(i); 
+	                break;
 	            } 
 			}
 			
@@ -68,5 +256,5 @@ public class Server {
 		}
 		
 	}
-	
+	*/
 }
